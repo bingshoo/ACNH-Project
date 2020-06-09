7e82143ecc8c4a6d16215d3e0ecf692647fb1580
Owner <x1f2b>
2020-06-08 23:14:26 -0700

    can display both fish and bugs but need to work on availability checking

--- script.js
+++ script.js
@@ -5,11 +5,30 @@ var allBugs = [];
 var fishData;
 var bugData;
 var bugFishTable;
+var fishTableEntries;
+
+// needs to make sure this finishes loading first 
+for(var fishID = 1; fishID <= 80; fishID++) {
+    fetch('http://acnhapi.com/v1/fish/' + fishID)
+    .then(response => response.json())
+    .then(data => allFish.push(data));
+}
+
+for(var bugID = 1; bugID <= 80; bugID++) {
+    fetch('http://acnhapi.com/v1/bugs/' + bugID)
+    .then(response => response.json())
+    .then(data => allBugs.push(data))
+    .catch(error => {
+        console.log("Error with grabbing bug data");
+        // alert("Reload the page");
+    });
+}
 
 document.addEventListener("DOMContentLoaded", () => {
     showTime();
     // onchange working but maybe remove the button
     bugFishTable = document.querySelector('#bug-fish-table');
+    fishTableEntries = bugFishTable.querySelector('tbody');
     document.querySelector("#time-travel-form").onchange = timeTravel;
     document.querySelector("#fish-toggle").onclick = displayCatchable;
     document.querySelector("#bug-toggle").onclick = displayCatchable;
@@ -20,18 +39,6 @@ document.addEventListener("DOMContentLoaded", () => {
         alert(`Welcome back to the present!`)
     };
     showTimeInterval = setInterval(showTime, 1000);
-
-    // needs to make sure this finishes loading first 
-    for(var fishID = 1; fishID <= 80; fishID++) {
-        fetch('http://acnhapi.com/v1/fish/' + fishID)
-        .then(response => response.json())
-        .then(data => allFish.push(data));
-    }
-
-
-    fetch('http://acnhapi.com/v1/bugs/')
-        .then(response => response.json())
-        .then(data => bugData = data);
 });
 
 function showTime() {
@@ -68,24 +75,85 @@ function displayCatchable() {
     let isNorthernHemisphere = document.querySelector("#northern-hemisphere").checked;
 
     console.log(`fish? ${isFishSelected} bug? ${isBugSelected} northern? ${isNorthernHemisphere} southern? ${!isNorthernHemisphere}`);
-    let fishTableEntries = bugFishTable.querySelector('tbody');
-    for(let fish of allFish) {
-        let newRow = fishTableEntries.insertRow();
-        let attributes = ['file-name', 'price', 'shadow', 'icon_uri', 'image_uri']
-        let newContent;
-        for(let i= 0; i<5; i++) {
-            let newCell = newRow.insertCell(i);
-            if (i < 3) {
-                newContent = document.createTextNode(fish[attributes[i]]);
-            } else {
-                // TODO need to fix the images 
-                let imageNode = document.createElement('img');
-                imageNode.src = fish[attributes[i]];
-                imageNode.alt = attributes[i];
-                newContent = imageNode;
-            }
-            newCell.append(newContent);
+    fishTableEntries.innerHTML = "";
+    if (isFishSelected)
+        display(allFish);
+
+    if (isBugSelected)
+        display(allBugs);
+}
+
+function display(collection) {
+    for(let element of collection) {
+        if(isAvailable(element)) {
+            insertIntoBugFishTable(element);
         }
     }
+    console.log(collection.length);
+}
+
+function insertIntoBugFishTable(bugFish) {
+    let newRow = fishTableEntries.insertRow();
+    let attributes = ['file-name', 'price', 'icon_uri']
+    let newContent;
+    for(let i= 0; i<3; i++) {
+        let newCell = newRow.insertCell(i);
+        if (i < 2) {
+            newContent = document.createTextNode(bugFish[attributes[i]]);
+        } else {
+            // TODO need to fix the images 
+            let imageNode = document.createElement('img');
+            imageNode.src = bugFish[attributes[i]];
+            imageNode.alt = attributes[i];
+            newContent = imageNode;
+        }
+        newCell.append(newContent);
+    }
+}
+
+// checks whether fish or bug is available depending on current time 
+function isAvailable(item) {
+    let isNorthernHemisphere = document.querySelector("#northern-hemisphere").checked;
+    if (item.availability.isAllDay && item.availability.isAllYear)
+        return true;
+
+    if (item.availability.isAllYear) {
+        return (checkTime(item.availability.time));
+    }
+    // TODO need to fix
+    return false;
+
+}
+
+function checkTime(timeIntervalString) {
+    let timeIntervalArray = timeIntervalString.split(" - ");
+    let firstTime = timeIntervalArray[0];
+    let secondTime = timeIntervalArray[1];
+    let possibleStartTime;
+    let possibleEndTime;
+    let startTime;
+    let endTime;
+
+    if(secondTime.includes("pm")) {
+        let temp = parseInt(firstTime.replace("pm", ""));
+        possibleStartTime = temp + 12;
+    } else {
+        possibleStartTime = parseInt(firstTime.replace("am", ""));
+    }
+
+    if(secondTime.includes("pm")) {
+        let temp = parseInt(secondTime.replace("pm", ""));
+        possibleEndTime = temp + 12;
+    } else {
+        possibleEndTime = parseInt(secondTime.replace("am", ""));
+    }
+
+    startTime = Math.min(possibleStartTime, possibleEndTime);
+    endTime = Math.max(possibleStartTime, possibleEndTime);
+
+    // need to double check this for when start time is in PM and end time is AM
+    let currentHour = currentDate.getHours();
+    return (currentHour >= startTime && currentHour < endTime);
+
 }
 
